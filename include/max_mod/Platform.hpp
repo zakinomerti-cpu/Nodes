837d533f668e4e55e45055bbbe28faa795938195
zakinomerti-cpu <zakinomerti@gmail.com>
2026-03-22 03:17:30 +0700

    Create Platform.hpp

---
+++ include/max_mod/Platform.hpp
@@ -0,0 +1,29 @@
+#ifndef _PLATFORM_HPP_
+#define _PLATFORM_HPP_
+
+typedef struct GLFWwindow GLFWwindow;
+
+#include <functional>
+
+class Platform {
+private:
+	int m_width;
+	int m_height;
+	char m_shouldClose;
+	GLFWwindow* nodesEditorWindow;
+	std::function<void()> m_onStart;
+	std::function<void(int width, int height)> m_onUpdate;
+	std::function<void()> m_onDestroy;
+public:
+	Platform(int width, int height, const char* title);
+	void onDestroy(std::function<void()> callback);
+	void onStart(std::function<void()> callback);
+	void onUpdate(std::function<void(int width, int height)> callback);
+	int getWidth();
+	int getHeingt();
+	void init();
+	void render();
+	char shouldClose();
+};
+
+#endif
